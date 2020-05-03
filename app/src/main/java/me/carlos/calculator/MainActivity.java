e6f430c0b7fa216c176094b548bd0003951d024b
Carlos Machado <carlosedumachado@edu.univali.br>
2020-05-03 09:08:32 -0300

    Operações adicionadas

--- app/src/main/java/me/carlos/calculator/MainActivity.java
+++ app/src/main/java/me/carlos/calculator/MainActivity.java
@@ -5,14 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     private final int ADICAO = 0, SUBTRACAO = 1, MULTIPLICACAO = 2, DIVISAO = 3;
-
     private EditText num1;
     private EditText num2;
     private TextView res;
@@ -22,38 +19,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        num1 = findViewById(R.id.etNumber1);
-        num2 = findViewById(R.id.etNumber2);
-        res = findViewById(R.id.tvResult);
+        num1 = findViewById(R.id.etNumero1);
+        num2 = findViewById(R.id.etNumero2);
+        res = findViewById(R.id.tvResultado);
     }
 
     private void selecionarOperacao(int op){
         String n1Str = num1.getText().toString().trim();
         String n2Str = num2.getText().toString().trim();
-
-        if(n1Str.equals("") || n2Str.equals("")){
-            Toast.makeText(this, "Informe os dois números.", Toast.LENGTH_SHORT).show();
-        }else{
+        if(n1Str.equals("") || n2Str.equals("")) {
+            Toast.makeText(this, "É necessário informar os dois números.", Toast.LENGTH_SHORT).show();
+        } else {
             String mensagemFinal = "Calculado com sucesso.";
             float r = 0F;
             float n1 = Float.valueOf(n1Str);
             float n2 = Float.valueOf(n2Str);
             switch (op){
-                case ADICAO:
-                    r = n1 + n2;
-                    break;
-                case SUBTRACAO:
-                    r = n1 - n2;
-                    break;
-                case MULTIPLICACAO:
-                    r = n1 * n2;
-                    break;
+                case ADICAO:        r = n1 + n2; break;
+                case SUBTRACAO:     r = n1 - n2; break;
+                case MULTIPLICACAO: r = n1 * n2; break;
                 case DIVISAO:
-                    if(n2 == 0F){
-                        mensagemFinal = "Não é possível dividir por zero!";
-                    } else {
-                        r = n1 / n2;
-                    }
+                    if(n2 == 0F) mensagemFinal = "Não é possível dividir por zero!";
+                    else r = n1 / n2;
                     break;
                 default:
                     r = -1;
@@ -64,19 +51,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void adicao(View v){
-
-    }
-
-    public void subtracao(View v){
-
-    }
-
-    public void multiplicacao(View v){
-
-    }
-
-    public void divisao(View v){
-        
-    }
+    public void adicao(View v) { selecionarOperacao(ADICAO); }
+    public void subtracao(View v) { selecionarOperacao(SUBTRACAO); }
+    public void multiplicacao(View v) { selecionarOperacao(MULTIPLICACAO); }
+    public void divisao(View v) { selecionarOperacao(DIVISAO); }
 }
