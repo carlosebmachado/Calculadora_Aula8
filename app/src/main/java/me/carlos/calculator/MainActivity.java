bef1b4608063e98732620f874d00daaa7e2e681c
Carlos Machado <carlosedumachado@edu.univali.br>
2020-05-04 07:23:16 -0300

    Finalizado

--- app/src/main/java/me/carlos/calculator/MainActivity.java
+++ app/src/main/java/me/carlos/calculator/MainActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private final int ADICAO = 0, SUBTRACAO = 1, MULTIPLICACAO = 2, DIVISAO = 3;
@@ -15,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText num2;
     private TextView res;
     private ListView historico;
+    private List listaHistorico;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
         num2 = findViewById(R.id.etNumero2);
         res = findViewById(R.id.tvResultado);
         historico = findViewById(R.id.lvHistorico);
+        listaHistorico = new ArrayList<String>();
+        ArrayAdapter<List> arrayAdapter = new ArrayAdapter<List>(this, android.R.layout.simple_list_item_1, listaHistorico);
+        historico.setAdapter(arrayAdapter);
     }
 
     private void selecionarOperacao(int op){
@@ -38,12 +46,25 @@ public class MainActivity extends AppCompatActivity {
             float n1 = Float.valueOf(n1Str);
             float n2 = Float.valueOf(n2Str);
             switch (op) {
-                case ADICAO:        r = n1 + n2; break;
-                case SUBTRACAO:     r = n1 - n2; break;
-                case MULTIPLICACAO: r = n1 * n2; break;
+                case ADICAO:
+                    r = n1 + n2;
+                    listaHistorico.add(n1Str + " + " + n2Str + " = " + r);
+                    break;
+                case SUBTRACAO:
+                    r = n1 - n2;
+                    listaHistorico.add(n1Str + " - " + n2Str + " = " + r);
+                    break;
+                case MULTIPLICACAO:
+                    r = n1 * n2;
+                    listaHistorico.add(n1Str + " * " + n2Str + " = " + r);
+                    break;
                 case DIVISAO:
-                    if(n2 == 0F) mensagemFinal = "Não é possível dividir por zero!";
-                    else r = n1 / n2;
+                    if(n2 == 0F){
+                        mensagemFinal = "Não é possível dividir por zero!";
+                    } else {
+                        r = n1 / n2;
+                        listaHistorico.add(n1Str + " / " + n2Str + " = " + r);
+                    }
                     break;
                 default:
                     r = -1;
@@ -60,6 +81,6 @@ public class MainActivity extends AppCompatActivity {
     public void divisao(View v) { selecionarOperacao(DIVISAO); }
 
     public void limparHistorico(View v) {
-
+        listaHistorico.clear();
     }
 }
